08bf3821ac39bc15d9c21fe70e1a441c919cafa0
MarekOchocki <marcopolo97@vp.pl>
2017-08-30 16:58:26 +0200

    Add methods in DepthProfile (#13)
    * Add methods in DepthProfile
    
    - range
    - FWHM
    - max_plat_ratio
    - distal_falloff
    - modulation
    - normalize
    
    * Add comment and rename some methods
    
    - remove range, because function width() from Profile does the same
    - explain modulation
    - rename fwhm
    
    * Add ran method and remove fwhm
    
    fwhm is already implemented in Profile

--- ibodata/depth_profile.py
+++ ibodata/depth_profile.py
@@ -1,6 +1,37 @@
+import numpy as np
+
 from beprof.profile import Profile
 
 
 class DepthProfile(Profile):
-    """TODO"""
-    pass
+
+    def ran(self, level=0.9):
+        return self.x_at_y(level, True)
+
+    def max_plat_ratio(self):
+        return self.y.max() / self.y[0]
+
+    def distal_falloff(self):
+        return self.x_at_y(0.1, True) - self.x_at_y(0.9, True)
+
+    def modulation(self, level):
+        """
+        if there's not y equal level from left x_at_y() returns NaN.
+        In that case instead of taking x_at_y(level), this function takes first x
+        """
+        if np.isnan(self.x_at_y(level)):
+            return self.x_at_y(level, True) - self.x[0]
+        else:
+            return self.x_at_y(level, True) - self.x_at_y(level)
+
+    def normalize(self, allow_cast=True):
+        """
+        Scale and translate y to be in range(0,1)
+        Normalize before calling other methods
+        """
+        self.y -= self.y.min()
+
+        if allow_cast:
+            self.y = self.y / self.y.max()
+        else:
+            self.y /= self.y.max()
