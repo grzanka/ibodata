f8811bead991805b2ec23b6091c743dcc3641100
MarekOchocki <marcopolo97@vp.pl>
2017-08-30 14:43:50 +0200

    Extend comments
    Additionally, change checking for NaN from d != d to np.isnan(d) to make it less confusing

--- ibodata/lateral_profile.py
+++ ibodata/lateral_profile.py
@@ -8,21 +8,21 @@ from beprof.profile import Profile
 class LateralProfile(Profile):
     def penumbra_right(self):
         """
-        In case of good data returns positive number
+        In case of good data returns float
         In case of corrupted data returns nan
         """
         return self.x_at_y(0.1, True) - self.x_at_y(0.9, True)
 
     def penumbra_left(self):
         """
-        In case of good data returns positive number
+        In case of good data returns float
         In case of corrupted data returns nan
         """
         return self.x_at_y(0.9) - self.x_at_y(0.1)
 
     def field_ratio(self, level):
         """
-        In case of good data returns positive number
+        In case of good data returns float
         Level has to be >= 0.0 and <= 1 or exception is raised
         In case of corrupted data returns nan
         """
@@ -32,7 +32,7 @@ class LateralProfile(Profile):
 
     def symmetry(self, level):
         """
-        In case of good data returns positive number
+        In case of good data returns float
         Level has to be >= 0 and <= 1 or exception is raised
         In case of corrupted data returns nan
         """
@@ -44,29 +44,31 @@ class LateralProfile(Profile):
 
     def flatness_50(self):
         """
+        Returns float
         In case of corrupted data returns nan
         Returns value between points if max/min value occurs on border
         """
         d = (self.penumbra_left() + self.penumbra_right()) / 2
-        if d != d:
+        if np.isnan(d):
             return np.nan
         left = self.x_at_y(0.5) + 2.0 * d
         right = self.x_at_y(0.5, True) - 2.0 * d
 
-        p_max = np.max(
-            np.append(self.y[np.logical_and(self.x <= right, self.x >= left)], [self.y_at_x(right), self.y_at_x(left)]))
-        p_min = np.min(
-            np.append(self.y[np.logical_and(self.x <= right, self.x >= left)], [self.y_at_x(right), self.y_at_x(left)]))
+        p_max = np.max(np.append(self.y[np.logical_and(self.x <= right, self.x >= left)],
+                                 [self.y_at_x(right), self.y_at_x(left)]))
+        p_min = np.min(np.append(self.y[np.logical_and(self.x <= right, self.x >= left)],
+                                 [self.y_at_x(right), self.y_at_x(left)]))
 
         return ((p_max - p_min) / (p_max + p_min)) * 100.0
 
     def flatness_90(self):
         """
+        Returns float
         In case of corrupted data returns nan
         Returns value between points if max/min value occurs on border
         """
         d = (self.penumbra_left() + self.penumbra_right()) / 2
-        if d != d:
+        if np.isnan(d):
             return np.nan
         left = self.x_at_y(0.9) + d
         right = self.x_at_y(0.9, True) - d
@@ -80,8 +82,10 @@ class LateralProfile(Profile):
 
     def asymmetry(self):
         """
+        If everything is fine returns float
         In case of corrupted data returns nan
         Add area between mid and the nearest points if there's no 0 value in self.x
+        area_left and area_right are DataSet and it propagates to result so it has to be converted to float
         """
         area_left = np.trapz(self.y[self.x <= 0], self.x[self.x <= 0])
         area_right = np.trapz(self.y[self.x >= 0], self.x[self.x >= 0])
@@ -95,10 +99,14 @@ class LateralProfile(Profile):
             area_right += np.trapz(np.append(self.y_at_x(0), self.y[right_index_arr[0]]),
                                   np.append([.0], self.x[right_index_arr[0]]))
 
-        return float(((area_left - area_right) / (area_left + area_right)) * 100.0)
+        result = ((area_left - area_right) / (area_left + area_right)) * 100.0
+
+        return float(result)
 
     def normalize(self, dt, allow_cast=True):
         """
+        Doesn't return anything
+        In case of corrupted data raises ValueError
         Translate y to bring y.min() to 0 (noise substraction) and then
         normalize to 1 over [-dt, +dt] area from the mid of the profile
         """
@@ -107,7 +115,7 @@ class LateralProfile(Profile):
         a = self.y.max() - self.y.min()
         a /= 2.0
         w = self.width(a)
-        if w != w:
+        if np.isnan(w):
             raise ValueError("Part of profile is missing.")
         mid = self.x_at_y(a) + w / 2.0
         self.x -= mid
